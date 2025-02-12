e3121ce1ced66ee974e58b6eb787d50f10faba29
rashidul191 <rashidul.191cse.gub@gmail.com>
2025-02-13 00:12:38 +0600

    basic where condition

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,13 +17,16 @@ class ProductController extends Controller
         // return Product::avg('price');
         // return Product::sum('price');
         // return Product::max('price');
-        return Product::min('price');
+        // return Product::min('price');
+
+        // return Product::select('title', 'price')->get();
+        // return Product::select('title')->distinct()->get();
+        return Product::where('price', '=', '390')->get();
     }
 
     public function productIncrement($id)
     {
         // return Product::where('id', $id)->increment('price', 50);
         return Product::where('id', $id)->decrement('price', 50);
-
     }
 }
